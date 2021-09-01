45a9d47360f8000c9b8ab3e2c64e4810ee6d5c91
Lucy-Glanville-Brown <lucyglanvillebrown@gmail.com>
2021-09-01 18:10:59 +0000

    Add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,6 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     runGame("addition");
-
 });
 
 /**
@@ -51,12 +50,13 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :-D");
+        incrementScore();
     }  else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
-
 }
 
 
@@ -76,14 +76,26 @@ function calculateCorrectAnswer() {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
-
 }
 
+
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
 
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
